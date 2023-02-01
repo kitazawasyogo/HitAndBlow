40f0531298d923c27452a11f6e860d4ceb79efb2
KITAZAWA Syogo <kitazawa@daniel-soft.com>
2023-02-01 14:34:03 +0900

    正解の4桁の数字をランダムで作成する機能追加

---
+++ HitAndBlow/HitAndBlowGame.vb
@@ -0,0 +1,36 @@
+﻿Public Class HitAndBlowGame
+
+
+    ''' <summary>
+    ''' 正解の4桁の数字を生成して返す
+    ''' </summary>
+    ''' <returns>正解の4桁の数字</returns>
+    Public Function MakeCorrectValue() As Char()
+
+        Const CORRECT_ARRAY_LENGTH As Integer = 4
+
+        Dim random As New Random()
+
+        Dim correctArray As Char() = {"0", "0", "0", "0"}
+
+        Dim arrayIndex As Integer = 0
+
+        While arrayIndex < CORRECT_ARRAY_LENGTH
+
+            Dim randomValue As Integer = random.Next(0, 9)
+
+            If Not correctArray.Contains(randomValue.ToString) Then
+
+                correctArray(arrayIndex) = randomValue.ToString
+                arrayIndex += 1
+
+            End If
+
+        End While
+
+        Return correctArray
+
+    End Function
+
+
+End Class
---
+++ HitAndBlowTest/HitAndBlowGameTest.vb
@@ -0,0 +1,36 @@
+﻿Imports HitAndBlow
+Imports NUnit.Framework
+
+Public Class HitAndBlowGameTest
+
+    Private gameTest As New HitAndBlowGame
+
+    <Test()> Public Sub 正解の値の桁数()
+
+        Dim actual As Char() = gameTest.MakeCorrectValue()
+
+        Assert.That(actual.Count, [Is].EqualTo(4))
+
+    End Sub
+
+    <Test()> Public Sub 正解の値が数字であるか()
+
+        Dim actual As Char() = gameTest.MakeCorrectValue()
+
+        Assert.IsTrue(IsNumeric(String.Join(",", actual)))
+
+    End Sub
+
+    <Test()> Public Sub 正解の値に重複がないか()
+
+        Dim actualArray As Char() = gameTest.MakeCorrectValue()
+
+        Dim actualList As IEnumerable(Of Char) = actualArray.Distinct()
+        Dim actual As List(Of Char) = actualList.ToList()
+
+        Assert.That(actual.Count, [Is].EqualTo(4))
+
+    End Sub
+
+End Class
+
